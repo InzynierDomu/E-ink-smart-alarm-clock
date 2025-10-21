eea5cf5210e8509bb8fa7e78ff3d7dc4fb68e60a
Inżynier Domu <inzynier.domu@gmail.com>
2025-10-21 13:42:31 +0200

    add print to weather view

--- src/weather_view.cpp
+++ src/weather_view.cpp
@@ -8,11 +8,47 @@ Weather_view::Weather_view(Screen* scr)
   // tempLabel = lv_label_create(screen->getLvObj());
 }
 
-void Weather_view::show(const Simple_weather& data)
+void Weather_view::show(const Weather_model& data)
 {
-  // Aktualizuj LVGL obiekty danymi
-  // String tempStr = String(data.temperature, 1) + "°C";
-  // String humStr = String(data.humidity, 1) + "%";
-  // lv_label_set_text(tempLabel, tempStr.c_str());
-  // lv_label_set_text(descLabel, data.description.c_str());
+  static lv_obj_t* tempLabelsMorning[] = {ui_labTempMorning, ui_labTempMorningDay1, ui_labTempMorningDay2, ui_labTempMorningDay3};
+  static lv_obj_t* tempLabelsAfternoon[] = {nullptr, ui_labTempAfternoonDay1, ui_labTempAfternoonDay2, ui_labTempAfternoonDay3};
+  static lv_obj_t* tempLabelsEvening[] = {nullptr, ui_labTempEveningDay1, ui_labTempEveningDay2, ui_labTempEveningDay3};
+  static lv_obj_t* weatherIcons[] = {ui_labWeatherIcon, ui_labWeatherIconDay1, ui_labWeatherIconDay2, ui_labWeatherIconDay3};
+
+  char temp_str[10];
+  Simple_weather forecast;
+  for (size_t i = 0; i < 4; ++i)
+  {
+    data.get_forecast(forecast, i);
+    if (tempLabelsMorning[i])
+    {
+      sprintf(temp_str, "%d℃", forecast.temperature_morning);
+      lv_label_set_text(tempLabelsMorning[i], temp_str);
+    }
+    if (i > 0 && tempLabelsAfternoon[i])
+    {
+      sprintf(temp_str, "%d℃", forecast.temperature_afternoon);
+      lv_label_set_text(tempLabelsAfternoon[i], temp_str);
+      sprintf(temp_str, "%d℃", forecast.temperature_evening);
+      lv_label_set_text(tempLabelsEvening[i], temp_str);
+    }
+    lv_label_set_text(weatherIcons[i], weather_icon_change(forecast.cloud_cover, forecast.precipitation));
+  }
+
+  sprintf(temp_str, "%d℃", forecast.temperature_afternoon);
+  lv_label_set_text(ui_labTempMorning, temp_str);
+}
+
+const char* Weather_view::weather_icon_change(int cloud_cover, int precipitation)
+{
+  if (cloud_cover > 30 && precipitation == 0)
+    return "ú";
+
+  if (precipitation < 5 && precipitation != 0)
+    return "û";
+
+  if (precipitation > 5)
+    return "ü";
+
+  return "ù";
 }
--- src/weather_view.h
+++ src/weather_view.h
@@ -6,9 +6,11 @@ class Weather_view
 {
   public:
   Weather_view(Screen* scr);
-  void show(const Simple_weather& data);
+  void show(const Weather_model& data);
 
   private:
+    const char* weather_icon_change(int cloud_cover, int precipitation);
+
   Screen* screen;
   // LVGL objects, np. lv_obj_t *tempLabel, *descLabel;
 };
