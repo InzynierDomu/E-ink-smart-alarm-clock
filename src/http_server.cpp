91178e1cadeb39db209326a36deb37c199130a95
Inżynier Domu <inzynier.domu@gmail.com>
2026-05-04 13:58:56 +0200

    merge html and css

--- src/http_server.cpp
+++ src/http_server.cpp
@@ -573,7 +573,9 @@ String HttpServer::buildPage()
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Konfiguracja - Inteligentny alarm na e-ink</title>
-    <link rel="stylesheet" href="/config_page_style.css">
+    <style>)rawHTML";
+  page += config_page_style_css;
+  page += R"rawHTML(</style>
 </head>
 <body>
     <div class="container">
